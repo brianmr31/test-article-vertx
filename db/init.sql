9f3f0d040369927428844409dbf161d75ec7900f
Brian Raharjo <brianmr31@gmail.com>
2019-11-16 22:49:43 +0700

    create db company

--- db/init.sql
+++ db/init.sql
@@ -4,10 +4,24 @@ CREATE TABLE public."setting" (
   setting_app_company varchar(255) NULL,
 	setting_name varchar(255) NULL,
 	setting_data varchar(255) NULL,
-  setting_type varchar(255) NULL,
+  setting_type varchar(255) NULL, -- NoPhone, NoWork, Email, Address, Logo, 
 	setting_created_by varchar(255) NULL,
 	setting_created_at timestamp NULL,
   setting_updated_by varchar(255) NULL,
   setting_updated_at timestamp NULL,
 	setting_deleted_flag bool NULL
+);
+-- DROP TABLE public."company";
+CREATE TABLE public."company" (
+	company_id bigserial PRIMARY KEY,
+	company_name varchar(255) NULL,
+	company_owner varchar(255) NULL,
+	company_status varchar(255) NULL,
+	company_valid_from timestamp NULL,
+	company_valid_to timestamp NULL,
+	company_created_by varchar(255) NULL,
+	company_created_at timestamp NULL,
+  company_updated_by varchar(255) NULL,
+  company_updated_at timestamp NULL,
+	company_deleted_flag bool NULL
 );
