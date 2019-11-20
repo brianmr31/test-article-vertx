689b10115d872192fbf88a3e6c12c44c754ddaa2
Brian Raharjo <brianmr31@gmail.com>
2019-11-20 08:37:59 +0700

    marge

--- src/main/java/corp/Br1aN/ctrl/article/category/handler/AddCategoryHandler.java
+++ src/main/java/corp/Br1aN/ctrl/article/category/handler/AddCategoryHandler.java
@@ -10,6 +10,8 @@ import io.vertx.ext.web.RoutingContext;
 
 import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Row;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -22,18 +24,27 @@ public class AddCategoryHandler implements Handler<RoutingContext> {
                                     "category_updated_by, category_updated_at, category_deleted_flag) VALUES( $1, $2, $3, $4, $5, $6, NOW(), null, null, false)";
 
   private JsonObject dataResponse = null;
+  private String parent = "0";
+  private int queue = 0;
 
   private PgPool pool = null;
 
   public AddCategoryHandler(PgPool pool){
     this.pool = pool;
+    this.dataResponse = null;
+    this.parent = "0";
+    this.queue = 0;
+  }
+  public void clear(){
+    this.dataResponse = null;
+    this.parent = "0";
+    this.queue = 0;
   }
   public void handle(RoutingContext context) {
     this.pool.getConnection( ar -> {
       JsonObject dataRequest = context.getBodyAsJson();
       HttpServerResponse response = context.response();
-      String parent = null;
-      int queue = 0;
+
       if( dataRequest.containsKey("name") == false ){
         this.dataResponse = new JsonObject().put("msg", "category name is params missing").put("code","err_category_add").put("data",false);
         response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
@@ -47,8 +58,8 @@ public class AddCategoryHandler implements Handler<RoutingContext> {
         response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
       }
       if( dataRequest.containsKey("parent_id") != false ){
-        parent = dataRequest.getString("parent_id");
-        queue = queue + 1;
+        this.parent = dataRequest.getString("parent_id");
+        // queue = queue + 1;
       }
       if( dataRequest.containsKey("username") == false ){
         this.dataResponse = new JsonObject().put("msg", "username is params missing").put("code","err_category_add").put("data",false);
@@ -57,22 +68,44 @@ public class AddCategoryHandler implements Handler<RoutingContext> {
 
       if (ar.succeeded()) {
         SqlConnection conn = ar.result();
-        Tuple data = Tuple.of(dataRequest.getValue("name"), dataRequest.getValue("type"), dataRequest.getString("app_company"),
-                  parent, queue, dataRequest.getValue("username"));
-        conn.preparedQuery( INSERT_CATEGORY, data, ar2 -> {
-          if (ar2.succeeded()) {
-            this.dataResponse = new JsonObject().put("msg", "ok").put("code","ok").put("data", false );
-            response.setStatusCode(200).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+        conn.preparedQuery("select category_id, category_name, category_queue from public.category where category_id=$1 limit 1", Tuple.of( Long.parseLong(parent) ), parent_res -> {
+          RowSet<Row> parent_categorys = parent_res.result();
+          if( parent_categorys.rowCount() != 0 ){
+            for (Row row_parent : parent_categorys) {
+              if( row_parent != null ){
+                this.queue = row_parent.getInteger(2) + 1 ;
+              }
+            }
+          }
+
+          System.out.println("Parent : "+this.queue);
+          Tuple data = Tuple.of(dataRequest.getValue("name"), dataRequest.getValue("type"), dataRequest.getString("app_company"),
+                    this.parent, this.queue, dataRequest.getValue("username"));
+          if (parent_res.succeeded()) {
+            conn.preparedQuery( INSERT_CATEGORY, data, ar2 -> {
+              if (ar2.succeeded()) {
+                this.dataResponse = new JsonObject().put("msg", "ok").put("code","ok").put("data", false );
+                response.setStatusCode(200).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+                this.clear();
+              }else{
+                System.out.println("Failure: " + ar2.cause().getMessage());
+                this.dataResponse = new JsonObject().put("msg", "data is missing ").put("code","data_is_missing").put("data",false);
+                response.setStatusCode(500).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+                this.clear();
+              }
+            });
           }else{
-            System.out.println("Failure: " + ar2.cause().getMessage());
+            System.out.println("Failure: " + parent_res.cause().getMessage());
             this.dataResponse = new JsonObject().put("msg", "data is missing ").put("code","data_is_missing").put("data",false);
             response.setStatusCode(500).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+            this.clear();
           }
         });
       }else{
         System.out.println("Failure: " + ar.cause().getMessage());
         this.dataResponse = new JsonObject().put("msg", "connection is error").put("code","err_connection").put("data",false);
         response.setStatusCode(500).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+        this.clear();
       }
     });
   }
--- src/main/java/corp/Br1aN/ctrl/article/category/handler/PutCategoryHandler.java
+++ src/main/java/corp/Br1aN/ctrl/article/category/handler/PutCategoryHandler.java
@@ -10,6 +10,8 @@ import io.vertx.ext.web.RoutingContext;
 
 import io.vertx.sqlclient.Tuple;
 import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Row;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,53 +22,70 @@ public class PutCategoryHandler implements Handler<RoutingContext> {
                                       "category_updated_at=Now() WHERE category_id=$1";
 
   private JsonObject dataResponse = null;
+  private String parent = "0";
+  private int queue = 0;
 
   private PgPool pool = null;
 
   public PutCategoryHandler(PgPool pool){
     this.pool = pool;
+    this.dataResponse = null;
+    this.parent = "0";
+    this.queue = 0;
   }
   public void handle(RoutingContext context) {
     this.pool.getConnection( ar -> {
       JsonObject dataRequest = context.getBodyAsJson();
       HttpServerResponse response = context.response();
 
-      String parent = null;
-      int queue = 0;
+      if( dataRequest.containsKey("name") == false ){
+        this.dataResponse = new JsonObject().put("msg", "category name is params missing").put("code","err_category_add").put("data",false);
+        response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+      }
+      if( dataRequest.containsKey("type") == false ){
+        this.dataResponse = new JsonObject().put("msg", "category data is params missing").put("code","err_category_add").put("data",false);
+        response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+      }
+      if( dataRequest.containsKey("app_company") == false ){
+        this.dataResponse = new JsonObject().put("msg", "category status to is params missing").put("code","err_category_add").put("data",false);
+        response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+      }
+      if( dataRequest.containsKey("parent_id") != false ){
+        this.parent = dataRequest.getString("parent_id");
+      }
+      if( dataRequest.containsKey("username") == false ){
+        this.dataResponse = new JsonObject().put("msg", "username is params missing").put("code","err_category_add").put("data",false);
+        response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+      }
 
       if (ar.succeeded()) {
-
-        if( dataRequest.containsKey("name") == false ){
-          this.dataResponse = new JsonObject().put("msg", "category name is params missing").put("code","err_category_add").put("data",false);
-          response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
-        }
-        if( dataRequest.containsKey("type") == false ){
-          this.dataResponse = new JsonObject().put("msg", "category data is params missing").put("code","err_category_add").put("data",false);
-          response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
-        }
-        if( dataRequest.containsKey("app_company") == false ){
-          this.dataResponse = new JsonObject().put("msg", "category status to is params missing").put("code","err_category_add").put("data",false);
-          response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
-        }
-        if( dataRequest.containsKey("parent_id") == false ){
-          parent = dataRequest.getString("parent_id");
-          queue = queue + 1;
-        }
-        if( dataRequest.containsKey("username") == false ){
-          this.dataResponse = new JsonObject().put("msg", "username is params missing").put("code","err_category_add").put("data",false);
-          response.setStatusCode(400).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
-        }
-
         SqlConnection conn = ar.result();
-        Tuple data = Tuple.of(Integer.parseInt(context.request().getParam("id")), dataRequest.getValue("name"), dataRequest.getValue("type"), dataRequest.getValue("app_company"),
-              parent, queue, dataRequest.getValue("username") );
-        conn.preparedQuery( PUT_CATEGORY, data, ar2 -> {
-          if (ar2.succeeded()) {
-            this.dataResponse = new JsonObject().put("msg", "ok").put("code","ok").put("data", false );
-            response.setStatusCode(200).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+        conn.preparedQuery("select category_id, category_name, category_queue from public.category where category_id=$1 limit 1", Tuple.of( Long.parseLong(this.parent) ), parent_res -> {
+          RowSet<Row> parent_categorys = parent_res.result();
+          if( parent_categorys.rowCount() != 0 ){
+            for (Row row_parent : parent_categorys) {
+              if( row_parent != null ){
+                this.queue = row_parent.getInteger(2) + 1 ;
+              }
+            }
+          }
+
+          if (parent_res.succeeded()) {
+            Tuple data = Tuple.of(Integer.parseInt(context.request().getParam("id")), dataRequest.getValue("name"), dataRequest.getValue("type"), dataRequest.getValue("app_company"),
+                  this.parent, this.queue, dataRequest.getValue("username") );
+            conn.preparedQuery( PUT_CATEGORY, data, ar2 -> {
+              if (ar2.succeeded()) {
+                this.dataResponse = new JsonObject().put("msg", "ok").put("code","ok").put("data", false );
+                response.setStatusCode(200).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+              }else{
+                System.out.println("Failure: " + ar2.cause().getMessage());
+                this.dataResponse = new JsonObject().put("msg", "data is missing ").put("code","data_is_missing").put("data",false);
+                response.setStatusCode(500).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
+              }
+            });
           }else{
-            System.out.println("Failure: " + ar2.cause().getMessage());
-            this.dataResponse = new JsonObject().put("msg", "data is missing ").put("code","data_is_missing").put("data",false);
+            System.out.println("Failure: " + parent_res.cause().getMessage());
+            this.dataResponse = new JsonObject().put("msg", "connection is error").put("code","err_connection").put("data",false);
             response.setStatusCode(500).putHeader("content-type", "application/json").end(this.dataResponse.encodePrettily());
           }
         });
